5c6266bf2e1ac95f602dc0a03a25eb8067600e1f
Wu-beining <24320125@hdu.edu.cn>
2025-03-11 21:26:19 +0800

    Update test_demo.py

--- test_demo.py
+++ test_demo.py
@@ -27,13 +27,13 @@ def select_model(args, device):
         model_path = os.path.join('model_zoo', 'team00_EFDN.pth')
         model = EFDN()
         model.load_state_dict(torch.load(model_path), strict=True)
-    elif model_id == 1:
+    elif model_id == 33:
         pass # ---- Put your model here as below ---
-        # from models.team01_[your_model_name] import [your_model_name]
-        # name, data_range = f"{model_id:02}_[your_model_name]", [255.0 / 1.0] # You can choose either 1.0 or 255.0 based on your own model
-        # model_path = os.path.join('model_zoo', 'team01_[your_model_name].pth')
-        # model = [your_model_name]()
-        # model.load_state_dict(torch.load(model_path), strict=True)
+        from models.team33_EagleSR import EagleSR
+        name, data_range = f"{model_id:33}_EagleSR", 255.0  # You can choose either 1.0 or 255.0 based on your own model
+        model_path = os.path.join('model_zoo', 'team3_EagleSR.pth')
+        model = EagleSR()
+        model.load_state_dict(torch.load(model_path), strict=True)
     else:
         raise NotImplementedError(f"Model {model_id} is not implemented.")
 
